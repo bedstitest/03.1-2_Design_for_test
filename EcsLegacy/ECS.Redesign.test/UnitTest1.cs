3163e3397d6da2c144cd2d5de38602b9553831a6
ate <atren985@gmail.com>
2022-09-15 11:15:05 +0200

    Made new test project for Redesign

---
+++ EcsLegacy/ECS.Redesign.test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using ECS.Redesign;
+
+namespace ECS.Redesign.test
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            var uut = new ECS();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
